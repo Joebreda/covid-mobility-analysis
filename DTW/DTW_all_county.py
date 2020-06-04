23acd2a0f579a590090a387985343b7a6926268c
Qiuyue Xue <qiuyuexue@Qiuyues-MacBook-Pro.local>
2020-06-04 03:00:27 -0400

    fix when mobiilty data are all 0

--- DTW/DTW_all_county.py
+++ DTW/DTW_all_county.py
@@ -29,23 +29,8 @@ def load_all_covid_confirmed_cases_data():
 def covid_cases_for_county(covid_data_dict, county_key_word):
     county_covid_cases = covid_data_dict[county_key_word]
     county_covid_cases_delta = np.diff(county_covid_cases)
-
-    fig = plt.figure(figsize=(24,8))
-    plt.plot(county_covid_cases_delta)
-    plt.xlabel("date")
-    plt.ylabel("new death cases")
-    plt.title("number of new covid cases in " + county_key_word)
-    fig.savefig('all_us_counties_results/' + county_key_word + '_new_confirmed_cases.png')
-    
     county_covid_cases_delta = moving_average(county_covid_cases_delta)
     county_covid_cases_delta = stats.zscore(county_covid_cases_delta)
-    fig = plt.figure(figsize=(24,8))
-    plt.plot(county_covid_cases_delta)
-    plt.xlabel("date")
-    plt.ylabel("new death cases")
-    plt.title("filtered number of new covid cases in " + county_key_word)
-    fig.savefig('all_us_counties_results/' + county_key_word + 'filtered_new_confirmed_cases.png')
-
     return county_covid_cases_delta
   
 def compute_dtw(fips, county_name, current_county_covid_cases_delta, major_county_covid_cases_delta, mobility_data, mobility_data_type_index):
@@ -55,17 +40,10 @@ def compute_dtw(fips, county_name, current_county_covid_cases_delta, major_count
 
     distance_with_itself = dtw_visualize.distance(current_county_covid_cases_delta, mobility_data)
     distance_with_major_city = dtw_visualize.distance(major_county_covid_cases_delta, mobility_data)
-
-    fig = plt.figure(figsize=(24,8))
-    plt.plot(current_county_covid_cases_delta)
-    plt.xlabel("date")
-    plt.ylabel("new death cases")
-    plt.title("in DTW filtered number of new covid cases in " + county_name)
-    fig.savefig('all_us_counties_results/' + county_name + '_filtered_new_confirmed_cases_dtw.png')
-
-    d, paths = dtw_visualize.warping_paths(current_county_covid_cases_delta, mobility_data, window=25, psi=2)
-    best_path = dtw_visualize.best_path(paths)
-    dtwvis.plot_warpingpaths(current_county_covid_cases_delta, mobility_data, paths, best_path, filename="all_us_counties_results/dtw_" + county_name +".png")
+    
+    # d, paths = dtw_visualize.warping_paths(current_county_covid_cases_delta, mobility_data, window=25, psi=2)
+    # best_path = dtw_visualize.best_path(paths)
+    # dtwvis.plot_warpingpaths(current_county_covid_cases_delta, mobility_data, paths, best_path, filename="all_us_counties_results/dtw_" + county_name +".png")
     
     with open('all_us_counties_results/confirmed_cases_vs_dtw_distance.csv','a') as csvfile:
         csvfile.write(str(fips) +","+ county_name + "," + str(distance_with_itself) + "," + str(distance_with_major_city) + "\n")
@@ -82,7 +60,7 @@ def main():
     mobility_array = np.zeros(101)
     mobility_array_index = 0
     
-    with open('Google_US_Mobility_Report_toyset.csv','r') as csvfile:
+    with open('Google_US_Mobility_Report.csv','r') as csvfile:
         data = csv.reader(csvfile, delimiter = ',')
         header = next(data)
         for line in data:
@@ -99,18 +77,13 @@ def main():
                         mobility_array_index = 0
                 else:
                     if current_county:
-                        try:
-                            current_county_covid_cases_delta = covid_cases_for_county(covid_data_dict, current_county)
-                            fips = fips_dict[current_county]
-                            fig = plt.figure(figsize=(24,8))
-                            plt.plot(mobility_array)
-                            plt.xlabel("date")
-                            plt.ylabel("mobility")
-                            plt.title("mobility in " + current_county)
-                            fig.savefig('all_us_counties_results/' + current_county + '_mobility.png')
-                            compute_dtw(fips, current_county, current_county_covid_cases_delta, NYC_covid_cases_delta,  mobility_array, mobility_data_type_index)
-                        except:
-                            pass
+                        if np.count_nonzero(mobility_array)>0:
+                            try:
+                                current_county_covid_cases_delta = covid_cases_for_county(covid_data_dict, current_county)
+                                fips = fips_dict[current_county]
+                                compute_dtw(fips, current_county, current_county_covid_cases_delta, NYC_covid_cases_delta,  mobility_array, mobility_data_type_index)
+                            except:
+                                pass
                     mobility_array_index = 0
                     mobility_array = np.zeros(101)
                     #some of the mobility data don't have residential mobility
@@ -125,8 +98,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
 
 
